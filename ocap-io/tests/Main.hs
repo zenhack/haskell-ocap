46148a278634f189a2a992be0d8c67792f0214d7
Ian Denhardt <ian@zenhack.net>
2020-09-10 13:55:45 -0400

    Add a test

--- ocap-io/tests/Main.hs
+++ ocap-io/tests/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Zhp
+import Control.Exception (SomeException, try)
+import OCap.IO
+import OCap.IO.Unsafe
 
 main :: IO ()
-main = pure ()
+main = do
+    -- Make sure that:
+    --
+    -- * Undefined can't be used as an IOKey
+    -- * getIOKey and unsafeIOKey both can.
+    Left _ <- tryRun undefined
+    Right () <- tryRun unsafeIOKey
+    Right () <- getIOKey >>= tryRun
+    pure ()
+
+tryRun :: IOKey -> IO (Either SomeException ())
+tryRun key = try $ runOCap $ runIO (pure ()) key
