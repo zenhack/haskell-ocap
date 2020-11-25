1fd2c576a1b1abb3e0e0f803884b2098aebd67aa
Ian Denhardt <ian@zenhack.net>
2020-11-25 05:55:42 -0500

    filesystem: type-level IOMode, hClose

--- ocap-filesystem/src/OCap/FileSystem.hs
+++ ocap-filesystem/src/OCap/FileSystem.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE Safe                  #-}
@@ -8,15 +11,18 @@ module OCap.FileSystem
 
     , Dir
     , Handle
+    , IOMode(..)
 
     , openFile
     , openBinaryFile
+    , hClose
 
     , hPutStr
     , hPutStrLn
     ) where
 
-import Zhp hiding (Handle, hPutStr, hPutStrLn, openBinaryFile)
+import Zhp hiding
+    (Handle, IOMode (..), hClose, hPutStr, hPutStrLn, openBinaryFile)
 
 import qualified Data.ByteString as BS
 import qualified System.IO       as SIO
@@ -33,10 +39,16 @@ data Dir = Dir
     , key     :: !IOKey
     }
 
-data Handle = Handle
+data Handle (r :: Bool) (w :: Bool) = Handle
     { handle :: SIO.Handle
     , key    :: !IOKey
     }
+
+data IOMode :: Bool -> Bool -> * where
+    ReadMode :: IOMode 'True 'False
+    WriteMode :: IOMode 'False 'True
+    AppendMode :: IOMode 'False 'True
+    ReadWriteMode :: IOMode 'True 'True
 
 instance Show Path where
     show (Path segs) = "pathFromSegments " <> show segs
@@ -60,24 +72,34 @@ resolvePathStr Dir{dirPath} path =
     let Path {pathSegments} = dirPath <> path in
     concatMap ("/"<>) pathSegments
 
-openFile :: Dir -> Path -> IOMode -> OCapIO Handle
+convertMode :: IOMode r w -> SIO.IOMode
+convertMode = \case
+    ReadMode      -> SIO.ReadMode
+    WriteMode     -> SIO.WriteMode
+    AppendMode    -> SIO.AppendMode
+    ReadWriteMode -> SIO.ReadWriteMode
+
+openFile :: Dir -> Path -> IOMode r w -> OCapIO (Handle r w)
 openFile dir@Dir{key} path mode = do
-    handle <- runIO key $ SIO.openFile (resolvePathStr dir path) mode
+    handle <- runIO key $ SIO.openFile (resolvePathStr dir path) (convertMode mode)
     pure Handle { handle, key }
 
-openBinaryFile :: Dir -> Path -> IOMode -> OCapIO Handle
+openBinaryFile :: Dir -> Path -> IOMode r w -> OCapIO (Handle r w)
 openBinaryFile dir@Dir{key} path mode = do
-    handle <- runIO key $ SIO.openBinaryFile (resolvePathStr dir path) mode
+    handle <- runIO key $ SIO.openBinaryFile (resolvePathStr dir path) (convertMode mode)
     pure Handle { handle, key }
 
-hPutStr :: Handle -> String -> OCapIO ()
+hClose :: Handle r w -> OCapIO ()
+hClose Handle {key, handle} = runIO key $ SIO.hClose handle
+
+hPutStr :: Handle r 'True -> String -> OCapIO ()
 hPutStr Handle {handle, key} = runIO key . SIO.hPutStr handle
 
-hPutStrLn :: Handle -> String -> OCapIO ()
+hPutStrLn :: Handle r 'True -> String -> OCapIO ()
 hPutStrLn Handle {handle, key} = runIO key . SIO.hPutStrLn handle
 
-hPut :: Handle -> BS.ByteString -> OCapIO ()
+hPut :: Handle r 'True -> BS.ByteString -> OCapIO ()
 hPut Handle {handle, key} = runIO key . BS.hPut handle
 
-hGet :: Handle -> Int -> OCapIO BS.ByteString
+hGet :: Handle 'True w -> Int -> OCapIO BS.ByteString
 hGet Handle {handle, key} = runIO key . BS.hGet handle
